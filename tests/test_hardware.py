75d02bed8b1bed58a913d185d28a0231ab5d0599
Daniel Oom <daoo314@gmail.com>
2017-04-09 15:02:32 +0200

    Mock RPi modules in setUp

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -2,48 +2,52 @@ import unittest
 from unittest.mock import MagicMock, call, patch
 from autodesk.model import Down, Up
 
-mock = MagicMock()
-mock.rpi = MagicMock()
-
-import sys
-sys.modules['RPi'] = mock.rpi
-sys.modules['RPi.GPIO'] = mock.rpi.GPIO
-
-from autodesk.hardware import Hardware
-
 class TestHardware(unittest.TestCase):
     def setUp(self):
-        mock.reset_mock()
-
         self.time_patcher = patch('time.sleep')
         self.time_sleep = self.time_patcher.start()
         self.addCleanup(self.time_patcher.stop)
 
+        self.rpi = MagicMock()
+        self.gpio = self.rpi.GPIO
+        import sys
+        sys.modules['RPi'] = self.rpi
+        self.addCleanup(sys.modules.pop, 'RPi')
+        sys.modules['RPi.GPIO'] = self.gpio
+        self.addCleanup(sys.modules.pop, 'RPi.GPIO')
+
+        from autodesk.hardware import Hardware
+        # The hardware module must also be removed since it is cached and
+        # maintains a reference to the mock RPi module.
+        self.addCleanup(sys.modules.pop, 'autodesk.hardware')
         self.hardware = Hardware(5, (0, 1))
 
     def test_hardware_setup(self):
         self.hardware.setup()
-        mock.rpi.GPIO.setmode.assert_called_with(mock.rpi.GPIO.BOARD)
-        expected = [
-            call(0, mock.rpi.GPIO.OUT),
-            call(1, mock.rpi.GPIO.OUT)
-        ]
-        self.assertEqual(mock.rpi.GPIO.setup.call_args_list, expected)
-        self.assertFalse(mock.rpi.GPIO.cleanup.called)
+        self.gpio.setmode.assert_called_with(self.gpio.BOARD)
+        self.gpio.setup.assert_has_calls([
+            call(0, self.gpio.OUT),
+            call(1, self.gpio.OUT)
+        ])
+        self.gpio.cleanup.assert_not_called()
 
     def test_hardware_cleanup(self):
         self.hardware.cleanup()
-        mock.rpi.GPIO.cleanup.assert_called_once()
-        mock.rpi.GPIO.setmode.assert_not_called()
+        self.gpio.cleanup.assert_called_once()
+        self.gpio.setmode.assert_not_called()
 
     def test_hardware_go_down(self):
         self.hardware.go(Down())
-        expected = [call(0, mock.rpi.GPIO.HIGH), call(0, mock.rpi.GPIO.LOW)]
-        self.assertEqual(mock.rpi.GPIO.output.call_args_list, expected)
+        self.gpio.output.assert_has_calls([
+            call(0, self.gpio.HIGH),
+            call(0, self.gpio.LOW)
+        ])
         self.time_sleep.assert_called_once_with(5)
 
     def test_hardware_go_up(self):
         self.hardware.go(Up())
-        expected = [call(1, mock.rpi.GPIO.HIGH), call(1, mock.rpi.GPIO.LOW)]
-        self.assertEqual(mock.rpi.GPIO.output.call_args_list, expected)
+        self.gpio.output.assert_has_calls([
+            call(1, self.gpio.HIGH),
+            call(1, self.gpio.LOW)
+        ])
         self.time_sleep.assert_called_once_with(5)
