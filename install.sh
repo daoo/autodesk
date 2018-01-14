73360b9953698aa36985d6886d8f64ea1ce29b83
Daniel Oom <oom.daniel@gmail.com>
2018-01-14 12:22:44 +0100

    Make service run in install dir

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@ sudo -u autodesk git clone https://github.com/daoo/autodesk.git
 sudo -u autodesk virtualenv venv
 sudo -u autodesk venv/bin/pip install ./autodesk uwsgi
 
+sudo -u autodesk ln -fsn autodesk/srv srv
+sudo -u autodesk cp autodesk/sys/default.yml config.yml
+
 cp autodesk/sys/autodesk.service /etc/systemd/system
 cp autodesk/sys/99-gpio.rules /etc/udev/rules.d
-
-sudo -u autodesk cp autodesk/sys/default.yml config.yml
 
 systemctl enable --now autodesk.service
