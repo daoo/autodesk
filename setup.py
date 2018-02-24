18924872b969a112fc56f6008673d3b9e36eb7c7
Daniel Oom <oom.daniel@gmail.com>
2018-02-24 14:32:03 +0100

    Move requests to tests_require

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     version='1.0',
     packages=['autodesk'],
     include_package_data=True,
-    install_requires=['aiohttp', 'aiohttp_jinja2', 'pyyaml', 'requests'],
+    install_requires=['aiohttp', 'aiohttp_jinja2', 'pyyaml'],
+    test_requires=['requests'],
     test_suite='tests',
     scripts=['bin/logger.py'],
 )
