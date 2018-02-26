5ab5d7f1b1fecd6095d8b358f14a7774be35efbf
Daniel Oom <oom.daniel@gmail.com>
2018-02-26 12:59:19 +0100

    Use sys/testing.yml with :memory: in end-to-end test
    On windows, the path of the NamedTemporaryFile cannot be used for some
    reason.

--- tests/test_endtoend.py
+++ tests/test_endtoend.py
@@ -1,33 +1,11 @@
 import requests
 import subprocess
-import tempfile
 import unittest
-
-config_template = """
-desk:
-  delay: 1
-  limits:
-    down: 5
-    up: 5
-  motor_pins:
-    down: 1
-    up: 2
-  light_pin: 3
-server:
-  database_path: ":memory:"
-  address: "localhost"
-  port: 8080
-"""
 
 
 class TestServer(unittest.TestCase):
     def setUp(self):
-        config_file = tempfile.NamedTemporaryFile()
-        with open(config_file.name, 'w') as file:
-            file.write(config_template)
-        self.addCleanup(config_file.close)
-
-        cmd = ['python', '-u', '-m', 'autodesk.server', config_file.name]
+        cmd = ['python', '-u', '-m', 'autodesk.server', 'sys/testing.yml']
         self.process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                         stderr=subprocess.DEVNULL,
                                         encoding='utf-8')
