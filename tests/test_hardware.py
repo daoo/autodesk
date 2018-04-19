c63b8370dfe6078bf6e26fe7fc54c7dacbe22c05
Daniel Oom <oom.daniel@gmail.com>
2018-04-19 20:40:24 +0200

    Add hardware create tests

---
+++ tests/test_hardware.py
@@ -0,0 +1,41 @@
+from autodesk.hardware import create_hardware
+from unittest.mock import patch, MagicMock
+import unittest
+
+def config(hardware):
+    return {
+        'desk': {
+            'hardware': hardware,
+            'delay': 5,
+            'motor_pins': { 'down': 1, 'up': 2 },
+            'light_pin': 3,
+        }
+    }
+
+
+class TestRaspberryPi(unittest.TestCase):
+
+
+    def test_create_hardware_raspberry_pi(self):
+        raspberrypi = MagicMock()
+
+        with patch.dict('sys.modules', {'autodesk.hardware.raspberrypi': raspberrypi}):
+            create_hardware(config('raspberrypi'))
+
+        raspberrypi.RaspberryPi.assert_called_once_with(5, (1, 2), 3)
+
+    def test_create_hardware_ft232h(self):
+        ft232h = MagicMock()
+
+        with patch.dict('sys.modules', {'autodesk.hardware.ft232h': ft232h}):
+            create_hardware(config('ft232h'))
+
+        ft232h.Ft232h.assert_called_once_with(5, (1, 2), 3)
+
+    def test_create_hardware_noop(self):
+        noop = MagicMock()
+
+        with patch.dict('sys.modules', {'autodesk.hardware.noop': noop}):
+            create_hardware(config('noop'))
+
+        noop.Noop.assert_called_once_with()
