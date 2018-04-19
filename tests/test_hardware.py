60cf71c4de0ec8ac3c00094d5b0e764a39319021
Daniel Oom <oom.daniel@gmail.com>
2018-04-19 20:46:18 +0200

    Formatting fixes

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -2,24 +2,27 @@ from autodesk.hardware import create_hardware
 from unittest.mock import patch, MagicMock
 import unittest
 
+
 def config(hardware):
     return {
         'desk': {
             'hardware': hardware,
             'delay': 5,
-            'motor_pins': { 'down': 1, 'up': 2 },
+            'motor_pins': {'down': 1, 'up': 2},
             'light_pin': 3,
         }
     }
 
 
+def patch_module(module, mock):
+    return patch.dict('sys.modules', {module: mock})
+
+
 class TestRaspberryPi(unittest.TestCase):
-
-
     def test_create_hardware_raspberry_pi(self):
         raspberrypi = MagicMock()
 
-        with patch.dict('sys.modules', {'autodesk.hardware.raspberrypi': raspberrypi}):
+        with patch_module('autodesk.hardware.raspberrypi', raspberrypi):
             create_hardware(config('raspberrypi'))
 
         raspberrypi.RaspberryPi.assert_called_once_with(5, (1, 2), 3)
@@ -27,7 +30,7 @@ class TestRaspberryPi(unittest.TestCase):
     def test_create_hardware_ft232h(self):
         ft232h = MagicMock()
 
-        with patch.dict('sys.modules', {'autodesk.hardware.ft232h': ft232h}):
+        with patch_module('autodesk.hardware.ft232h', ft232h):
             create_hardware(config('ft232h'))
 
         ft232h.Ft232h.assert_called_once_with(5, (1, 2), 3)
