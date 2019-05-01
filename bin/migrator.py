3a9ced5eb6823f79e69a8f44e73b90876f939b25
Daniel Oom <oom.daniel@gmail.com>
2019-05-01 09:43:12 +0200

    Fix migrator session conversion

--- bin/migrator.py
+++ bin/migrator.py
@@ -19,11 +19,13 @@ new.execute(
 
 for desk in desks.itertuples():
     state = 'up' if desk.state == 1 else 'down'
+    print('INSERT INTO desk values({0}, {1})'.format(desk.date, state))
     new.execute('INSERT INTO desk values(?, ?)', (desk.date, state))
 
 for session in sessions.itertuples():
     state = 'active' if session.active == 1 else 'inactive'
-    new.execute('INSERT INTO session values(?, ?)', (desk.date, state))
+    print('INSERT INTO session values({0}, {1})'.format(session.date, state))
+    new.execute('INSERT INTO session values(?, ?)', (session.date, state))
 
 new.commit()
 
