ffdb2771f446d577e56982accc8be7a9e14e706c
Daniel Oom <oom.daniel@gmail.com>
2019-05-01 09:52:53 +0200

    Move database to a more appropriate location

--- bin/start-autodesk.sh
+++ bin/start-autodesk.sh
@@ -3,7 +3,7 @@
 export AUTODESK_ADDRESS="127.0.0.1"
 export AUTODESK_PORT="8080"
 export AUTODESK_CONFIG="./config/ft232h.yml"
-export AUTODESK_DATABASE="/tmp/autodesk.db"
+export AUTODESK_DATABASE="~/.local/share/autodesk/autodesk.db"
 
 source ./venv/bin/activate
 
