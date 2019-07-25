81463abd97a2b958bcf13cab3eb0ad256c28517d
Daniel Oom <oom.daniel@gmail.com>
2019-07-25 16:29:23 +0200

    Pin dependency versions
    Pin dependency versions, allowing only minor upgrades.

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ setuptools.setup(
     },
     include_package_data=True,
     install_requires=[
-        'aiohttp',
-        'aiohttp_jinja2',
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'pyyaml'
+        'aiohttp>=3.5.4, <3.6',
+        'aiohttp_jinja2>=1.1.2, <1.2',
+        'matplotlib>=3.1.1, <3.2',
+        'numpy>=1.16.4, <1.17',
+        'pandas>=0.25, <0.26',
+        'pyyaml>=5.1.1, <5.2'
     ],
 )
