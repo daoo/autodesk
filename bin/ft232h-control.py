679236478d456a2479873569c287be53379ccb04
Daniel Oom <oom.daniel@gmail.com>
2020-04-24 21:08:31 +0200

    Update control test script to use pyftdi
    Update control test script to use the i2c controller from pyftdi. This
    makes the gpio-tester script obsolete. Also, clean up the list devices
    script a bit.

--- bin/ft232h-control.py
+++ bin/ft232h-control.py
@@ -1,24 +1,35 @@
-import Adafruit_GPIO.FT232H as FT232H
+#!/usr/bin/env python
+from pyftdi.i2c import I2cController
 import sys
 import time
 
 
-def translate(pin, delay):
-    FT232H.use_FT232H()
-    device = FT232H.FT232H()
+def translate(gpio, pin, delay):
+    pin_mask = gpio.pins | 1 << pin
+    direction = gpio.direction | 1 << pin
+    gpio.set_direction(pin_mask, direction)
 
-    device.setup(pin, FT232H.GPIO.OUT)
-
-    device.output(pin, FT232H.GPIO.HIGH)
+    value = gpio.read(with_output=True)
+    gpio.write((value | 1 << pin) & gpio.direction)
     time.sleep(delay)
-    device.output(pin, FT232H.GPIO.LOW)
-    device.close()
+    value = gpio.read(with_output=True)
+    gpio.write(value & ~(1 << pin) & gpio.direction)
 
 
+if len(sys.argv) != 3:
+    sys.stderr.write('Usage: {0} pin delay\n'.format(sys.argv[0]))
+    sys.exit(1)
+
+pin = int(sys.argv[1])
+delay = int(sys.argv[2])
+
+controller = I2cController()
 try:
-    pin = int(sys.argv[1])
-    delay = int(sys.argv[2])
-    translate(pin, delay)
+    controller.configure("ftdi://ftdi:ft232h/1", frequency=1e6)
+    gpio = controller.get_gpio()
+    translate(gpio, pin, delay)
 except ValueError:
     sys.stderr.write("Usage: {} PIN SECONDS\n".format(sys.argv[0]))
     sys.exit(1)
+finally:
+    controller.terminate()
---
+++ bin/ft232h-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from pyftdi.ftdi import Ftdi
+
+ft232h_pid = (Ftdi.PRODUCT_IDS[Ftdi.FTDI_VENDOR]['ft232h'])
+id_tuple = (Ftdi.FTDI_VENDOR, ft232h_pid)
+
+devices = Ftdi.find_all([id_tuple])
+if devices:
+    for device in devices:
+        print(device)
+else:
+    print('No FT232H devices found.')
