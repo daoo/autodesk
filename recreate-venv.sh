9c0296d65f0f7d7ef979e2168b98df2dd063095e
Daniel Oom <oom.daniel@gmail.com>
2020-04-26 12:36:05 +0200

    Add requirements for dev environment

---
+++ recreate-venv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ue
+
+if [[ -n "${VIRTUAL_ENV+}" ]]; then
+  echo "Error: venv active, please deactivate first."
+  exit 1
+fi
+rm -rf "./venv/"
+python -m venv "./venv"
+./venv/bin/pip install --upgrade pip
+./venv/bin/pip install \
+  -r requirements.txt \
+  -r requirements-ci.txt \
+  -r requirements-dev.txt
