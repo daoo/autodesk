4d9ae4b6f2364f5cf952bd727fc487e2dc0a59bc
Daniel Oom <oom.daniel@gmail.com>
2022-11-18 08:54:03 +0100

    Disable state updates in RDP sessions

--- bin/autodesk-activate.ps1
+++ bin/autodesk-activate.ps1
@@ -1,6 +1,11 @@
-curl.exe `
-  --connect-timeout 300 `
-  --data active `
-  --header 'Content-Type: text/plain' `
-  --request PUT `
-  http://localhost:8080/api/session
+$remote = qwinsta.exe $env:UserName | Out-String
+if ($remote.contains("rdp-tcp")) {
+  Write-Host "RDP session, exiting."
+} else {
+  curl.exe `
+    --connect-timeout 300 `
+    --data active `
+    --header 'Content-Type: text/plain' `
+    --request PUT `
+    http://localhost:8080/api/session
+}
