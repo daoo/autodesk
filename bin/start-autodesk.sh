9275a41d58afbc94e779b65e6d750938a4040004
Daniel Oom <oom.daniel@gmail.com>
2024-10-29 20:55:18 +0100

    Change ~ to $HOME in start script

--- bin/start-autodesk.sh
+++ bin/start-autodesk.sh
@@ -3,6 +3,6 @@
 export AUTODESK_ADDRESS="127.0.0.1"
 export AUTODESK_PORT="7380"
 export AUTODESK_CONFIG="./config/ft232h.yml"
-export AUTODESK_DATABASE="~/.local/share/autodesk/autodesk.db"
+export AUTODESK_DATABASE="$HOME/AppData/Local/autodesk/autodesk.db"
 
 uv run autodesk
