5d3f67faf3c92eab6ea4206bddfaab254656399c
Daniel Oom <oom.daniel@gmail.com>
2025-10-07 15:34:35 +0200

    Proper logging in logger

--- bin/logger.py
+++ bin/logger.py
@@ -8,21 +8,31 @@
 # ]
 # ///
 
+import logging
 import sys
-from datetime import datetime
 
 import requests
 from gi.repository import GLib
 from pydbus import SystemBus
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+)
+
 
 def notify(url: str, active: bool):
+    logging.info(f"notify {url} {active}")
     state = b"active" if active else b"inactive"
-    requests.put(url, data=state, headers={"Content-Type": "text/plain"})
+    try:
+        result = requests.put(url, data=state, headers={"Content-Type": "text/plain"})
+        logging.info(f"result {result}")
+    except Exception as error:
+        logging.error(error)
 
 
 def properties_handler(url: str, interface, changed, invalidated):
-    print(datetime.now(), interface, changed, invalidated)
+    logging.info(f"handler {interface} {changed} {invalidated}")
     if interface != "org.freedesktop.login1.Session":
         return
     if "Active" in changed:
@@ -35,7 +45,7 @@ def program(url: str):
     bus = SystemBus()
     login = bus.get("org.freedesktop.login1")
     for _, _, _, _, path in login.ListSessions():
-        print(f"Connecting to {path}")
+        logging.info(f"connecting {path}")
         sessionbus = bus.get("org.freedesktop.login1", path)
         sessionbus.PropertiesChanged.connect(
             lambda interface, changed, invalidated: properties_handler(
