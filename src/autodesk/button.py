4197a8b98ec714530fe7c8e1cca500e0b2b31afa
Daniel Oom <oom.daniel@gmail.com>
2026-04-05 15:26:25 +0200

    Type hint button

--- src/autodesk/button.py
+++ src/autodesk/button.py
@@ -1,13 +1,17 @@
+from autodesk.application.autodeskservice import AutoDeskService
+from autodesk.hardware.types import InputPin
+
+
 class Button:
-    def __init__(self, pin, autodeskservice):
+    def __init__(self, pin: InputPin, autodeskservice: AutoDeskService) -> None:
         self.pin = pin
         self.autodeskservice = autodeskservice
-        self.buttondown = False
+        self._button_down = False
 
-    def poll(self):
+    def poll(self) -> None:
         value = self.pin.read()
-        if value == 1 and not self.buttondown:
+        if value == 1 and not self._button_down:
             self.autodeskservice.toggle_session()
-            self.buttondown = True
+            self._button_down = True
         elif value == 0:
-            self.buttondown = False
+            self._button_down = False
