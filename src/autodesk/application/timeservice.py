8acb54504aa17861a72584e537deab267752fbd9
Daniel Oom <oom.daniel@gmail.com>
2026-04-05 15:34:25 +0200

    Use UTC for now()

--- src/autodesk/application/timeservice.py
+++ src/autodesk/application/timeservice.py
@@ -1,12 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 MIN_TIME = datetime(1970, 1, 1)
+UTC = timezone.utc  # noqa: UP017
 
 
 class TimeService:
     @property
     def min(self) -> datetime:
-        return MIN_TIME
+        return MIN_TIME.replace(tzinfo=UTC)
 
     def now(self) -> datetime:
-        return datetime.now()
+        return datetime.now(UTC)
