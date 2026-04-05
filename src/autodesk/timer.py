6a4d4e272dedaf20e4e7ebb340b34c5a91d58c64
Daniel Oom <oom.daniel@gmail.com>
2026-04-05 14:35:30 +0200

    Type hint timer

--- src/autodesk/timer.py
+++ src/autodesk/timer.py
@@ -1,13 +1,14 @@
+import asyncio
 import logging
 from collections.abc import Callable
 from datetime import timedelta
 
 
 class Timer:
-    def __init__(self, loop) -> None:
+    def __init__(self, loop: asyncio.AbstractEventLoop) -> None:
         self.logger = logging.getLogger("timer")
         self.loop = loop
-        self.timer = None
+        self.timer: asyncio.TimerHandle | None = None
 
     def schedule(self, delay: timedelta, callback: Callable[[], None]) -> None:
         self.logger.info("scheduling in %s", delay)
@@ -16,7 +17,7 @@ class Timer:
         self.timer = self.loop.call_later(delay.total_seconds(), callback)
 
     def cancel(self) -> None:
-        self.logger.info("cancling")
+        self.logger.info("cancelling")
         if self.timer:
             self.timer.cancel()
             self.timer = None
